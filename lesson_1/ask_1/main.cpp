fa3c643de092093f5ae779ec6fb6b87542717e53
Anthony Veaudry <anthonyveaudry@gmail.com>
2016-10-15 20:42:55 +0300

    add lesson one projects

---
+++ lesson_1/ask_1/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout << "Anthony Veaudry";
+	return 0;
+}
---
+++ lesson_1/ask_3/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout << "1" << "2" << "3" << "4" << "5" << endl;
+	cout << "1" << endl << "2" << endl << "3" << endl << "4" << endl << "5" <<endl;
+	return 0;
+}
