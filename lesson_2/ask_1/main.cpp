8950abe5d44996a06fad7d1c6b9853b052775c99
Anthony Veaudry <anthonyveaudry@gmail.com>
2016-10-17 10:31:35 +0300

    add lesson 1 and 2

---
+++ lesson_2/ask_1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	int money;
+
+	cin >> money;
+	cout << "test";
+	return 0;
+}
---
+++ lesson_2/ask_2/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout << "Anthony Veaudry" << endl << "Limenas Thassos 64004" << endl;
+	return 0;
+}
